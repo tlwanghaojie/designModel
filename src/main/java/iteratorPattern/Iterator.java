6d3d190e2fb6a41258e831f3232f26a0979c1fa3
wanghj <2726870846@qq.com>
2021-12-13 21:22:17 +0800

    增加注释

--- src/main/java/iteratorPattern/Iterator.java
+++ src/main/java/iteratorPattern/Iterator.java
@@ -11,8 +11,8 @@ public interface Iterator {
 	 * description 判断是否存在下一个元素
 	 *
 	 * @return boolean
-	 * author WangHaojie
-	 * createTime 2021/12/13 18:53
+	 * @author WangHaojie
+	 * @createTime 2021/12/13 18:53
 	 **/
 	public abstract boolean hasNext();
 
